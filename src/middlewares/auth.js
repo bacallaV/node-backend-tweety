8f39837cb6af08ba0e06f6214916b2fabe5fbecc
bacallaV <valdohalo3@hotmail.com>
2024-02-26 16:32:40 -0600

    refactor: :recycle: translate error messages

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 function Auth (req, res, next) {
 	const token = req.headers.authorization?.split(' ')[1];
-	if(!token) return res.status(401).json('Sin autorización');
+	if(!token) return res.status(401).json('Unauthorized');
 
 	const jwtPayload = jwt.verify(token, 'my-key');
 
-	if(!jwtPayload) return res.status(403).json('Token inválido');
+	if(!jwtPayload) return res.status(403).json('Invalid token');
 
 	next();
 }
